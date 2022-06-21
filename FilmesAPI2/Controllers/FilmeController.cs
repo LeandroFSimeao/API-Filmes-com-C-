210adc11d378b0eefa8899c93ef688e2f470604d
LeandroFSimeao <leandrosimeao@yahoo.com.br>
2022-06-21 16:10:46 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI2/Controllers/FilmeController.cs
@@ -0,0 +1,44 @@
+﻿using FilmesAPI2.Data;
+using FilmesAPI2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FilmesAPI2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private FilmeContext _context;
+
+        public FilmeController(FilmeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            _context.Filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmePorID), new { Id = filme.Id }, filme);
+        }
+
+        [HttpGet]
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(_context.Filmes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorID(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ FilmesAPI2/Data/FilmeContext.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI2.Data
+{
+    public class FilmeContext : DbContext
+    {
+
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+
+    }
+}
